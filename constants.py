f2b01593a43f9d8ecfa702e74c7843d9b5189ace
Vrotki <51521287+Vrotki@users.noreply.github.com>
2024-04-23 13:59:17 -0500

    Created combined notebook

--- constants.py
+++
@@ -1,3 +0,0 @@
-
-kingdoms = ['arc', 'plm', 'rod', 'phg', 'pri', 'vrt', 'bct', 'pln', 'inv', 'mam', 'vrl']
-kingdom_names = ['archaea', 'plasmid', 'rodent', 'bacteriophage', 'primate', 'vertebrate', 'bacteria', 'plant', 'invertebrate', 'mammal', 'virus']
